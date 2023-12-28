f99affdb380c1cfc97bfab3d3340566584aab181
MSEali <m.serajian@ufl.edu>
2023-12-28 02:51:03 -0500

    Googletest updated

--- src/trimmer.cpp
+++ src/trimmer.cpp
@@ -1,19 +1,17 @@
-// trimmer.cpp
 #include "trimmer.h"
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <filesystem>
 
 FileNotFoundError::FileNotFoundError(const std::string &msg) : std::runtime_error(msg) {}
 EmptyFileError::EmptyFileError(const std::string &msg) : std::runtime_error(msg) {}
 DirectoryNotFoundError::DirectoryNotFoundError(const std::string &msg) : std::runtime_error(msg) {}
 
-
-void modify_text_file(const std::string &input_path, const std::string &output_path)
-{
+void modify_text_file(const std::string &input_path, const std::string &output_directory) {
+    // Read content from the input file
     std::ifstream input_file(input_path);
-    if (!input_file.is_open())
-    {
+    if (!input_file.is_open()) {
         std::cerr << "Error opening input file." << std::endl;
         return;
     }
@@ -22,33 +20,28 @@ void modify_text_file(const std::string &input_path, const std::string &output_p
     buffer << input_file.rdbuf();
     std::string content = buffer.str();
 
-    size_t last_letter_index = std::string::npos;
-    for (size_t i = content.size(); i > 0; --i)
-    {
-        if (std::isalpha(content[i - 1]))
-        {
-            last_letter_index = i - 1;
-            break;
-        }
+    // Find the index of the last alphabetic character
+    size_t last_letter_index = content.find_last_of("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    if (last_letter_index != std::string::npos) {
+        last_letter_index += 1; // Include the last letter in the substring
+        content.erase(last_letter_index);
     }
 
-    std::string modified_content;
-    if (last_letter_index != std::string::npos)
-    {
-        modified_content = content.substr(0, last_letter_index + 1);
-        modified_content.erase(modified_content.find_last_not_of("\n") + 1) += "\n";
-    }
-    else
-    {
-        modified_content = content + "\n";
-    }
+    // Create the output directory if it doesn't exist
+    std::filesystem::create_directories(output_directory);
 
+    // Extract the filename from the input path
+    std::filesystem::path input_file_path_obj(input_path);
+    std::string output_file_name = input_file_path_obj.filename();
+
+    // Create the output path by concatenating the output directory and the filename
+    std::string output_path = output_directory + output_file_name;
+
+    // Write modified content to the output file
     std::ofstream output_file(output_path);
-    if (!output_file.is_open())
-    {
+    if (!output_file.is_open()) {
         std::cerr << "Error opening output file." << std::endl;
         return;
     }
-
-    output_file << modified_content;
+    output_file << content;
 }
--- test/test_process_arguments.cpp
+++ test/test_process_arguments.cpp
@@ -3,6 +3,7 @@
 #include "trimmer.h"
 #include <iostream>
 #include <unistd.h>  // Include for getcwd
+#include <fstream>
 
 // ANSI escape code for blue text
 #define ANSI_BLUE "\033[34m"
@@ -10,40 +11,52 @@
 // ANSI escape code to reset text color
 #define ANSI_RESET "\033[0m"
 
-class ProcessArgumentsTest : public ::testing::Test {
+class TrimmerTest : public ::testing::Test {
 protected:
-    void callProcessArguments(int argc, char* argv[], std::string& input_file, std::string& output_dir) {
+    void callProcessArguments(int argc, const char* argv[], std::string& input_file, std::string& output_dir) {
         try {
-            process_arguments(argc, argv, input_file, output_dir);
+            process_arguments(argc, const_cast<char**>(argv), input_file, output_dir);
         } catch (const std::exception& e) {
             exception_message = e.what();
         }
     }
 
-    std::string exception_message;
-};
-
-TEST_F(ProcessArgumentsTest, CorrectArguments) {
-    char cwd[1024];
-    if (getcwd(cwd, sizeof(cwd)) != NULL) {
-        std::cout << "Current working directory: " << cwd << std::endl;
+    void SetUp() override {
+        // Prepare a temporary input file
+        const std::string input_content = "This is a sample text file.";
+        input_path = "test_input.txt";
+        std::ofstream input_file(input_path);
+        input_file << input_content;
+        input_file.close();
     }
 
+    void TearDown() override {
+        // Clean up: Remove temporary files
+        std::remove(input_path.c_str());
+        std::remove(output_path.c_str());
+    }
+
+    std::string exception_message;
+    std::string input_path;
+    std::string output_path;
+};
+
+TEST_F(TrimmerTest, CorrectArguments) {
     int argc = 5;
-    char* argv[] = {"program_name", "-i", "../../data/genomes/1773.1547.fna", "-o", "."};
+    const char* argv[] = {"program_name", "-i", "/absolute/path/to/your/project/data/genomes/1773.1547.fna", "-o", "."};
 
     std::string input_file, output_dir;
     callProcessArguments(argc, argv, input_file, output_dir);
 
     // Update the expectation for exception_message
-    EXPECT_EQ("Error: File '../../data/genomes/1773.1547.fna' not found.", exception_message);
+    EXPECT_EQ("Error: File '/absolute/path/to/your/project/data/genomes/1773.1547.fna' not found.", exception_message);
 
     EXPECT_EQ(".", output_dir);
 }
 
-TEST_F(ProcessArgumentsTest, MissingInputOption) {
+TEST_F(TrimmerTest, MissingInputOption) {
     int argc = 3;
-    char* argv[] = {"program_name", "-o", "output_directory"};
+    const char* argv[] = {"program_name", "-o", "output_directory"};
 
     std::string input_file, output_dir;
     callProcessArguments(argc, argv, input_file, output_dir);
@@ -52,9 +65,9 @@ TEST_F(ProcessArgumentsTest, MissingInputOption) {
     EXPECT_EQ("Usage: program_name -i <input_directory> -o <output_directory>", exception_message);
 }
 
-TEST_F(ProcessArgumentsTest, MissingOutputOption) {
+TEST_F(TrimmerTest, MissingOutputOption) {
     int argc = 3;
-    char* argv[] = {"program_name", "-i", "../../data/genomes/1773.1547.fna"};
+    const char* argv[] = {"program_name", "-i", "/absolute/path/to/your/project/data/genomes/1773.1547.fna"};
 
     std::string input_file, output_dir;
     callProcessArguments(argc, argv, input_file, output_dir);
@@ -62,6 +75,42 @@ TEST_F(ProcessArgumentsTest, MissingOutputOption) {
     EXPECT_NE("", exception_message);
     EXPECT_EQ("Usage: program_name -i <input_directory> -o <output_directory>", exception_message);
 }
+
+// TEST_F(TrimmerTest, ModifyTextFile) {
+//     // Define file paths
+//     const std::string input_file_path = "/absolute/path/to/your/project/data/genomes/1773.1547.fna";
+//     const std::string output_directory = "/absolute/path/to/your/project/data/genomes/ModifyTextFile_output/";
+
+//     // Check if the input file exists
+//     std::ifstream input_check(input_file_path);
+//     ASSERT_TRUE(input_check.is_open()) << "Failed to open input file: " << input_file_path;
+//     input_check.close();
+
+//     // Call the modify_text_file function
+//     modify_text_file(input_file_path, output_directory);
+
+//     // Verify the output file content
+//     std::string output_path = output_directory + "1773.1547.fna";
+//     std::ifstream output_file(output_path);
+//     std::ifstream correct_file("/absolute/path/to/your/project/data/genomes/correct_1773.1547.fna");
+
+//     // Check if both files are open
+//     ASSERT_TRUE(output_file.is_open()) << "Failed to open output file: " << output_path;
+//     ASSERT_TRUE(correct_file.is_open()) << "Failed to open correct file: /absolute/path/to/your/project/data/genomes/correct_1773.1547.fna";
+
+//     // Read content of both files
+//     std::stringstream output_buffer, correct_buffer;
+//     output_buffer << output_file.rdbuf();
+//     correct_buffer << correct_file.rdbuf();
+
+//     // Compare the content
+//     EXPECT_EQ(output_buffer.str(), correct_buffer.str())
+//         << "Output content does not match the correct content.";
+
+//     // Close the files
+//     output_file.close();
+//     correct_file.close();
+// }
 
 int main(int argc, char** argv) {
     std::cout << ANSI_BLUE << "Running tests for argument_parser.cpp using Google Test..." << ANSI_RESET << std::endl;
